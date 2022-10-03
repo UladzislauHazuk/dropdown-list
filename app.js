0d91cdfeec106baa795ff62d43ab30807374eb3c
VladislavGazuk <vladislav.gazuk@gmail.com>
2022-10-03 20:30:12 +0200

    add index, app, style

---
+++ app.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.btn');
+const city = document.querySelector('.city');
+
+btn.addEventListener('click', () => {
+    if (city.hidden) {
+        city.hidden = false;
+        document.querySelector('.img').style = `background-image: url(./assets/Chevron.svg);`;
+    } else {
+        city.hidden = true;
+        document.querySelector('.img').style = `background-image: url(./assets/Chevron2.svg);`;
+    }
+});
